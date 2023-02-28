612d237eb157a2055da1e07d6c885e1c7350888b
gautte <s362066@oslomet.no>
2023-02-28 22:35:37 +0100

    Started on the client

--- oblig1/client.py
+++ oblig1/client.py
@@ -7,3 +7,40 @@ should take command line arguments specifying the server IP address or host name
 the server is listening, and the path at which the requested object is stored at the server. The following
 is an input command format to run the client. client.py server host server port filename
 """
+
+import sys
+from socket import*
+"""
+sys.argv is a list in Python that contains the command-line arguments passed to the script. When a Python script is 
+executed, the command-line arguments passed are stored in the sys.argv list.
+"""
+serverHost = sys.argv[1]
+serverPort = int(sys.argv[2])
+filename = sys.argv[3]
+
+# TCP client socket
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# Connecting to server
+clientSocket.connect((serverHost, serverPort))
+
+# Sending the HTTP get request to the server
+request = "GET /" + filename + " HTTP/1.1\r+nHost: " + serverHost + "\r\n\r\n"
+clientSocket.send(request.encode())
+
+# Receiving HTTP response form server
+response = clientSocket.recv(1024)
+
+output = ""
+
+while len(response) > 0:
+    response = clientSocket.recv(4096).decode()
+    output += response
+print(output)
+
+
+# Printing message
+print(response.decode())
+
+# Closing socket
+clientSocket.close()
