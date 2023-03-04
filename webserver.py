345c9905d1df997f140e9d5b20f0191af3ddcf8d
gautte <s362066@oslomet.no>
2023-03-04 15:50:27 +0100

    Added comments to the server file

--- webserver.py
+++ webserver.py
@@ -19,57 +19,45 @@ def handle_requests(connectionSocket):
         # Receive the request message from the client
         message = connectionSocket.recv(2048).decode()
         filename = message.split()[1][1:]
-        # f = open(filename[1:])
         # the name of the file includes "/", to remove it, [1:] is used to slice the file name from the second character
-        with open(filename, 'rb') as f:
-            outputdata = f.read()
-        # f.close()
+        with open(filename, 'rb') as f:         #Open file in "binary mode" - read content as bytes
+            outputdata = f.read()               #Read content of filename as f
+
 
         # Send one HTTP header line into socket
-        connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
+        connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())               #HTTP response status line
         connectionSocket.send('Content-Type: text/html\r\n'.encode())
         connectionSocket.send(('Content-Length: %d\r\n' % len(outputdata)).encode())
-        connectionSocket.send('\r\n'.encode())
+        connectionSocket.send('\r\n'.encode())                          #End of the response header
 
         # Send the content of the requested file to the client
 
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i:i + 1])
-            # Loop that sends characters from outputdata
-            """
-            In Python, when you slice a string or a sequence, you can use the start:stop:step notation. 
-            The i:i+1 slice notation used in outputdata[i:i+1] creates a new sequence containing a single element, 
-            which is the ith element of outputdata.
+            # Loop that sends characters from outputdata one byte at a time from 0 to the length of outputdata
 
-            The reason why the slice notation is used here is that connectionSocket.send() requires a bytes-like 
-            object as its input. outputdata is a string, so each character must be converted to a byte using the 
-            encode() method before it is sent.
-
-            Alternatively, outputdata[i:i+1].encode() could be used instead of outputdata[i:i+1], which would encode 
-            each slice as bytes before sending it to connectionSocket.send().
-
-            """
     except IOError:
         # Send response message for file not found
-        connectionSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
+        connectionSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())   #HTTP response status if there are errors trying to run the code above
         connectionSocket.send('Content-Type: text/html\r\n'.encode())
         connectionSocket.send('\r\n'.encode())
         connectionSocket.send('<html><body><h1>404 Not Found</h1></body></html>\r\n'.encode())
     finally:
-        connectionSocket.close()
+        connectionSocket.close()        #Ensuring that the program exits
 
 
 serverSocket = socket(AF_INET, SOCK_STREAM)  # Server socket
-serverPort = 6789
-serverHost = '127.0.0.1'
-serverSocket.bind(('', serverPort))
+serverPort = 6789                            #Server port
+serverHost = '127.0.0.1'                       #Server host
+serverSocket.bind(('', serverPort))             #Associating server socket with specific network interface
 
-serverSocket.listen(5)
+serverSocket.listen(5)                      #Maximum number of queued connecteiions
 
 # Establish the connection
 while True:
+    #Loop that listens for incoming connections
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
-
+    #Create a new thread that handles requests
     t = threading.Thread(target=handle_requests, args=(connectionSocket,))
     t.start()
